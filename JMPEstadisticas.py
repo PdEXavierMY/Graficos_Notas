aa65f783a451c0af99d788247022d75ee1aa7818
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-04-20 19:33:16 +0200

    .

--- JMPEstadisticas.py
+++ JMPEstadisticas.py
@@ -30,15 +30,12 @@ class JMPEstadisticas:
     def __init__(self,caracteristica):
         self.caracteristica = caracteristica
 
-
     def calculoMediaAritmetica(self):
 
         n = self.caracteristica.count()
         sumaValoresObservaciones = 0
-        mediaAritmetica = 0
         for valorObservacion in self.caracteristica:
             sumaValoresObservaciones = sumaValoresObservaciones + valorObservacion
-
         mediaAritmetica = sumaValoresObservaciones / n
         return mediaAritmetica
 
